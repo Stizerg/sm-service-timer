bb559ae44d276d8fb16e7b230a9c6ad07dc52e96
Stizerg <105415949+Stizerg@users.noreply.github.com>
2024-07-22 18:50:40 +1000

    Update timer-start-12h.sh

--- timer-start-12h.sh
+++ timer-start-12h.sh
@@ -11,20 +11,20 @@ commands=(
 times=("6:00" "7:00" "8:00")
 
 # Array of dates
-dates=("2024-07-22" "2024-08-05")
+dates=("2024-08-05" "2024-08-19" "2024-09-02")
 
 # Get current date and time
-current_date=$(date +%Y-%m-%d)
-current_time=$(date +%H:%M)
+current_date=$(date +%s)
 
 # For each date
 for date in "${dates[@]}"; do
+    scheduled_date=$(date -d"$date ${times[i]}" +%s)
     # For each service
     for i in "${!commands[@]}"; do
         # Define a name for the timer
         timer_name="smh_${i}_${date//[-]/}"
         # If the date is today and the time is in the past, schedule the service to run within 1 minute
-        if [[ "$date" == "$current_date" && "${times[i]}" < "$current_time" ]]; then
+        if (( scheduled_date < current_date )); then
             cmd="systemd-run --user --unit=$timer_name --on-active='1min' -G --no-ask-password ${commands[i]}"
         else
             cmd="systemd-run --user --unit=$timer_name --on-calendar='${date} ${times[i]}' -G --no-ask-password ${commands[i]}"
